695be960c38c3b4cc31d2732e3a0ffdf0a2abfeb
ununennium <isaac.cv123@gmail.com>
2022-04-23 20:22:14 -0400

    feat: initial commit
    The file "app.js" has been created.
    All packages have been installed.

---
+++ app.js
@@ -0,0 +1,31 @@
+// Express
+const express = require('express');
+const app = express();
+
+// Morgan
+const morgan = require('morgan');
+
+// Security packages
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+// Middlewares
+app.set('trust proxy', 1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000,
+        max: 100
+    })
+);
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(xss());
+app.use(morgan('dev'));
+
+// Port
+app.set('port', 3000);
+
+app.listen(app.get('port'), console.log(`Server running on port : ${app.get('port')}`));
